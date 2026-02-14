aa7534937da29db5464a6771aed5ea0d181dff4b
Christian <christian.mercado9@upr.edu>
2026-02-13 23:54:27 -0400

    Add files via upload

---
+++ admin-config.js
@@ -0,0 +1,5 @@
+// Admin password hash (SHA-256). Update only if you want to change the fixed password.
+window.MR_MERCADO_ADMIN_HASH = "486367b3b0855505eab0ab3b5918127be4edafe08c5c9479e35ec5934fddb1cb";
+
+// Site access password hash (SHA-256).
+window.MR_MERCADO_SITE_HASH = "93e7432e75a2e2244e91744953e22c9c1d7ebfa8d8415f418b8be4981d34caf7";
