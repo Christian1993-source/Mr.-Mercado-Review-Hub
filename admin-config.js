5652ea1ddb53543ab14ebcf75930cb6e1fbc617d
Christian <christian.mercado9@upr.edu>
2026-02-13 23:25:19 -0400

    Delete admin-config.js

--- admin-config.js
+++
@@ -1,2 +0,0 @@
-// Admin password hash (SHA-256). Update only if you want to change the fixed password.
-window.MR_MERCADO_ADMIN_HASH = "486367b3b0855505eab0ab3b5918127be4edafe08c5c9479e35ec5934fddb1cb";
